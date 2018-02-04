c3fe474a0d0c711a5e0d2dd1f4e098834ab22234
maxsarandev <maxsarandev@gmail.com>
2018-02-04 17:36:38 +0000

    Section 2 COMPLETE (and server)

--- Tracker/data_extractor.py
+++ Tracker/data_extractor.py
@@ -4,20 +4,22 @@
 
 # Purpose: extract all useful information from a text dump
 
-import datetime
+import datetime, requests
 
 class Extractor:
-    def __init__(self):
+    def __init__(self,id):
         self.file_location = ""
         self.string_return = ""
         self.time_stamp = datetime.datetime.now()
+        self.id = id  # give the tracker a unique id
 
     # simply generates a new timestamp
     def newTimeStamp(self):
        self.time_stamp = datetime.datetime.now()
 
     # Custom method
-    def extractData(self, file_path):
+    # Data extraction below
+    def extractData(self, file_path, file_prefix):
         with open(file_path) as f:
             lines = f.readlines()
 
@@ -29,7 +31,7 @@ class Extractor:
                     # the line contains the MAC address
 
                     # fetch the mac
-                    self.string_return += str(count)
+                    self.string_return += str(count) + ", "
                     # splice the string, to return the mac only
                     self.string_return += line_x[3:line_x.find("(")]
 
@@ -42,7 +44,7 @@ class Extractor:
                 # look for the signal strength
                 if("signal:" in line_x):
                     # splice the string to return the signal only
-                    self.string_return += line_x[line_x.find(":"):line_x.find("/n")]
+                    self.string_return += line_x[line_x.find(":")+1:line_x.find("/n")]
 
                     # for ease of use
                     self.string_return += ","
@@ -50,7 +52,7 @@ class Extractor:
                 # look for the ESSID
                 if("SSID:" in line_x):
                     # splice and push to string
-                    self.string_return += line_x[line_x.find(":"):line_x.find("/n")]
+                    self.string_return += line_x[line_x.find(":")+1:line_x.find("/n")]
 
                     # add the timestamp and end line
                     self.newTimeStamp()
@@ -61,6 +63,28 @@ class Extractor:
             f.close()
 
             # open the save file, push, close
-            f = open(str(self.time_stamp)+"_Capture.txt", 'a')
+            f = open(self.id + file_prefix, 'a')
             f.write(self.string_return)
             f.close()
+
+    # Server Health-check
+    def checkHealth(self, srv_address):
+        # Send a test check
+        try:
+            ret_string = requests.get("http://" + srv_address,
+                                      timeout=2)
+
+            return 200
+        except requests.exceptions.ConnectTimeout,\
+                requests.exceptions.ReadTimeout:
+            return 503  # connection timeout
+
+    # Server Upload file
+    def uploadFile(self, srv_address, file):
+        files = {'file': open(file, 'rb')}  # define the file
+
+        # push the file via the address
+        r = requests.post("http://" + srv_address,
+                          files=files, timeout=5)
+
+        r.close()  # close the conn
--- Tracker/exec.py
+++ Tracker/exec.py
@@ -2,19 +2,36 @@
 # January-June 2018
 # All Rights Reserved
 
-import os
+import os, requests
 from data_extractor import Extractor
 
 # Check what OS the system is running (Unix - posix, Win - nt)
 if (os.name == "posix"):
+    server_address = "192.168.0.25:8000"
+    file_name = "test.txt"
+    file_prefix = "_Capture.txt"
+    net_adapter = "wlan0"
+
     # launch the command and save to test.txt
-    res_string = os.system("iw dev wlan0 scan | cat > test.txt")
+    res_string = os.system("iw dev "+ net_adapter +" scan | cat > "+file_name)
 
     # deploy
-    ex1 = Extractor()
+    ex1 = Extractor("0")
 
     # extract the data
-    ex1.extractData("test.txt")
+    ex1.extractData(file_name, file_prefix)
+
+    conn_status = ex1.checkHealth(server_address)
+
+    # if a connection can be established with the server...
+    if(conn_status == 200):
+        # attempt transfer
+        try:
+            ex1.uploadFile(server_address, ex1.id+file_prefix)
+
+            print "Upload OK"
+        except ex1 as requests.ConnectionError:
+            print "Connection error" + ex1
 
 elif (os.name == "nt"):
     print "Windows is not supported"
